8c6c7bc68a6ccd13a0b1397c294564a828eb9faa
louiss0 <louisshelton0@gmail.com>
2024-05-31 21:52:17 -0400

    chore: Setup icons and unocss styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import Icons from 'unplugin-icons/vite';
+import UnoCSS from 'unocss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [UnoCSS(), Icons({ compiler: "vue3" }), vue()],
 })
